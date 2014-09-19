5206ca7eb88c3f9ff0400edbeeccc8db707c2123
Derek-lai- <derek_lai_@hotmail.com>
2014-09-18 22:02:44 -0400

    error checking

--- Java/Talon.java
+++ Java/Talon.java
@@ -1,15 +1,15 @@
 public class Talon {
 
-	public static char[] encode(long lat, long lon) {
-		char val_1[] = _encode(lat);
-		char val_2[] = _encode(lon);
-		char first = val_1[0];
-		char second = val_1[1];
-		char third = val_2[0];
-		char fourth = val_2[1];
+	public static byte[] encode(long lat, long lon) {
+		byte val_1[] = _encode(lat);
+		byte val_2[] = _encode(lon);
+		byte first = val_1[0];
+		byte second = val_1[1];
+		byte third = val_2[0];
+		byte fourth = val_2[1];
 
-		char[] r = new char[5];
-		r[0] = (char)"\u2641";
+		byte[] r = new byte[5];
+		r[0] = "\u2641";
 		r[1] = first;
 		r[2] = second;
 		r[3] = third;
@@ -18,11 +18,11 @@ public class Talon {
 		return r;
 	}
 
-	public static char[] decode(String code) {
+	public static byte[] decode(String code) {
 		return -1;
 	}
 
-	private static char[] _encode(long loc){
+	private static byte[] _encode(long loc){
 		long val;
 		if (loc < 0){
 			val = 0;
@@ -38,18 +38,16 @@ public class Talon {
 			}
 		}
 		val += Math.abs(loc) * 10000000;
-		val = Long.toHexString(Long.parseLong(val));
-		String val_1 = val.substring(0 , 4);
-		String val_2 = val.substring(4);
-		char first = (char)(Integer.parseInt(val_1, 16));
-		char second = (char)(Integer.parseInt(val_2, 16));
-		char[] result = new char[2];
-		result[0] = first;
-		result[1] = second;
+		String valString = Long.toHexString(val);
+		String val_1 = valString.substring(0 , 4);
+		String val_2 = valString.substring(4);
+		byte[] first = String.valueOf((Integer.parseInt(val_1, 16))).getBytes("UTF8");
+		byte[] second = String.valueOf((Integer.parseInt(val_2, 16))).getBytes("UTF8");
+		byte[] result = {first, second};
 		return result;	
 	}
 
-	private static char[] _decode(int first, int second) {
+	private static byte[] _decode(int first, int second) {
 		return -1;
 	}
 
