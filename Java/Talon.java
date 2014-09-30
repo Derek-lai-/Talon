b771342ddd9c45a3c83872dca64bc839a690f074
Derek-lai- <derek_lai_@hotmail.com>
2014-09-30 16:49:43 -0400

    _encode done? maybe, encode

--- Java/Talon.java
+++ Java/Talon.java
@@ -1,12 +1,14 @@
 public class Talon {
 
 	public static byte[] encode(long lat, long lon) {
-		byte val_1[] = _encode(lat);
-		byte val_2[] = _encode(lon);
-		byte first = val_1[0];
-		byte second = val_1[1];
-		byte third = val_2[0];
-		byte fourth = val_2[1];
+		byte[][] val_1 = _encode(lat);
+		byte[][] val_2 = _encode(lon);
+
+		byte first = val_1[0][];
+		byte second = val_1[1][];
+		byte third = val_2[0][];
+		byte fourth = val_2[1][];
+
 
 		byte[] r = new byte[5];
 		r[0] = "\u2641";
@@ -19,10 +21,10 @@ public class Talon {
 	}
 
 	public static byte[] decode(String code) {
-		return -1;
+		return null;
 	}
 
-	private static byte[] _encode(long loc){
+	private static byte[][] _encode(long loc){
 		long val;
 		if (loc < 0){
 			val = 0;
@@ -38,17 +40,25 @@ public class Talon {
 			}
 		}
 		val += Math.abs(loc) * 10000000;
+
 		String valString = Long.toHexString(val);
+
 		String val_1 = valString.substring(0 , 4);
 		String val_2 = valString.substring(4);
-		byte[] first = String.valueOf((Integer.parseInt(val_1, 16))).getBytes("UTF8");
-		byte[] second = String.valueOf((Integer.parseInt(val_2, 16))).getBytes("UTF8");
-		byte[] result = {first, second};
+
+		Integer temp_1 = Integer.parseInt(val_1, 16);
+		Integer temp_2 = Integer.parseInt(val_2, 16);
+
+		byte first[] = (String.valueOf(temp_1)).getBytes("UTF8");
+		byte second[] = (String.valueOf(temp_2)).getBytes("UTF8");
+
+		byte[][] result = {first, second};
+
 		return result;	
 	}
 
 	private static byte[] _decode(int first, int second) {
-		return -1;
+		return null;
 	}
 
 }
